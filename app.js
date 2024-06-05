7b0c42ec01daeb00ab30bf0fb23a2eb447dd92f5
rishabhm05 <rishabh.mehta@williamoneilindia.com>
2024-06-05 11:05:45 +0530

    Added Frontend Url

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let usersroom={};
 let fileStreams={};
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:3000",
+        origin:["http://localhost:3000/","https://filesharing-vert.vercel.app/"],
         methods:["GET","POSTS"],
         credentials:true
     },
